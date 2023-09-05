20ff31d3de6c1df96b78febb03f9aa5c041bb3e4
Luthfi-dev <97874380+Luthfi-dev@users.noreply.github.com>
2023-09-05 22:00:00 +0700

    deletepage.js

--- pages/page.js
+++ pages/page.js
@@ -1,10 +1,1 @@
-// pages/page.js
-import RootLayout from "../src/components/Admin/AdminLayout";
 
-export default function Page() {
-  return (
-    <RootLayout>
-      <>halaman tampilan konten || cooming soon</>
-    </RootLayout>
-  );
-}
