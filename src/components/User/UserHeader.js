e304f5a349c381366c057f60d8558952412f777d
Luthfi-dev <luthfi2048@gmail.com>
2023-09-26 08:53:40 +0700

    perbaki a to Link

--- src/components/User/UserHeader.js
+++ src/components/User/UserHeader.js
@@ -87,7 +87,7 @@ const AdminHeader = () => {
       {/* SECTION SEARCH */}
       {/* END ECTION SEARCH */}
       <div className="d-flex align-items-center justify-content-between">
-        <a href="/" className="logo d-flex align-items-center">
+        <Link href="/" className="logo d-flex align-items-center">
           <Image
             src="/assets/img/logo.png"
             alt="logo epic"
@@ -105,7 +105,7 @@ const AdminHeader = () => {
               height={80}
             />
           </span>
-        </a>
+        </Link>
         <i
           className="bi bi-list toggle-sidebar-btn"
           onClick={tampilkanToggleSidebar}
@@ -131,9 +131,9 @@ const AdminHeader = () => {
       <nav className="header-nav ms-auto">
         <ul className="d-flex align-items-center">
           <li className="nav-item d-block d-lg-none">
-            <a className="nav-link nav-icon search-bar-toggle ">
+            <span className="nav-link nav-icon search-bar-toggle ">
               <FullScreenDialog />
-            </a>
+            </span>
           </li>
           {/* <!-- End Search Icon--> */}
 
@@ -169,61 +169,6 @@ const AdminHeader = () => {
             </Dropdown>
             {/* <!-- End Profile Iamge Icon --> */}
 
-            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-              <li className="dropdown-header">
-                <h6>Luthfi-dev</h6>
-                <span>Fullstack</span>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-
-              <li>
-                <a
-                  className="dropdown-item d-flex align-items-center"
-                  href="users-profile.html"
-                >
-                  <i className="bi bi-person"></i>
-                  <span>My Profile</span>
-                </a>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-
-              <li>
-                <a
-                  className="dropdown-item d-flex align-items-center"
-                  href="users-profile.html"
-                >
-                  <i className="bi bi-gear"></i>
-                  <span>Account Settings</span>
-                </a>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-
-              <li>
-                <a
-                  className="dropdown-item d-flex align-items-center"
-                  href="pages-faq.html"
-                >
-                  <i className="bi bi-question-circle"></i>
-                  <span>Need Help?</span>
-                </a>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-
-              <li>
-                <a className="dropdown-item d-flex align-items-center" href="#">
-                  <i className="bi bi-box-arrow-right"></i>
-                  <span>Sign Out</span>
-                </a>
-              </li>
-            </ul>
             {/* <!-- End Profile Dropdown Items --> */}
           </li>
           {/* <!-- End Profile Nav --> */}
